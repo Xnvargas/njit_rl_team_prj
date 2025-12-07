4a8055337f7abb1f44284ca737a353b9273bc417
RobCaamano <caamanor@gmail.com>
2025-12-06 22:54:24 -0500

    plotting code

--- scripts/run_voyager.py
+++ scripts/run_voyager.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+import time
 from voyager import Voyager
+
+t0 = time.perf_counter()
 
 load_dotenv()
 MC_PORT = os.environ.get("MC_PORT")
@@ -11,3 +14,5 @@ voyager = Voyager(
 )
 
 voyager.learn()
+
+print(f"\033[32mVoyager execution elapsed time={time.perf_counter() - t0:.2f}s\033[0m")
--- voyager/utils/__init__.py
+++ voyager/utils/__init__.py
@@ -1,3 +1,4 @@
 from .file_utils import *
 from .json_utils import *
 from .record_utils import EventRecorder
+from .plot_discoveries import *
